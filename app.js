c0b3900a9c586ff3241a1a768a88a658edaeacc9
SaraTaharani <144036947+SaraTaharani@users.noreply.github.com>
2024-05-12 21:27:51 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
 const path = require('path');
 const cors = require('cors');
 const postsRouter= require("./routes/postsRouter")
